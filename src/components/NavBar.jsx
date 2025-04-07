062d384b09ab77edf8e8afcea60990157fe20b94
Kaung <104748803+KaungPhyoThant@users.noreply.github.com>
2025-04-07 14:02:03 +0630

    minor changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
             <img src={logo} alt="logo" width={48} height={48} className="rounded-lg mx-2 w-25"/>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <a href="https://github.com/CertifiedDepressedBoy"><FaGithub/></a>
+            <a href="https://github.com/KaungPhyoThant"><FaGithub/></a>
             <a href="https://www.linkedin.com/in/kaung-phyo-thant-926160338/"><FaLinkedin/></a>
         </div>
     </nav>
